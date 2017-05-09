2a7c36e85efd216f8e3472f290ee400820059a29
allyface <ally.b.kendall@gmail.com>
2017-05-09 11:44:08 -0700

    complete compact array method with all tests passing

--- src/array.js
+++ src/array.js
@@ -1,7 +1,6 @@
-export default class LodashArray {
+export default class _Array {
 
-// _.chunk(array, [size=1]): Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
-
+  // _.chunk(array, [size=1]): Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
   static chunk(array, size=1) {
     let chunkedArray = []
     for (let i = 0; i < array.length; i++) {
@@ -13,5 +12,16 @@ export default class LodashArray {
       }
     }
     return chunkedArray
+  }
+
+  // _.compact(array): Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+  static compact(array) {
+    let compactArray = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i]) {
+        compactArray.push(array[i])
+      }
+    }
+    return compactArray
   }
 }
