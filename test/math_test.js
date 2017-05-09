3dea3f8106ae08e2bb0dbf226f7c9826ddd762cb
allyface <ally.b.kendall@gmail.com>
2017-05-09 16:19:14 -0700

    complete floor math method with all tests passing

--- test/math_test.js
+++ test/math_test.js
@@ -16,13 +16,27 @@ describe('_Math.ceil', () => {
     expect(_Math.ceil(4.006)).to.equal(5)
   })
 
-  it('Takes a negative integer and rounds to corresponding place.', function() {
+  it('Takes a negative integer for precision and rounds to corresponding place.', function() {
     expect(_Math.ceil(6040, -2)).to.equal(6100)
   })
 })
 
-describe.only('_Math.divide', () => {
+describe('_Math.divide', () => {
   it('Divides two numbers.', function() {
     expect(_Math.divide(6, 4)).to.equal(1.5)
+  })
+})
+
+describe.only('_Math.floor', () => {
+  it('Computes number rounded down to precision.', function() {
+    expect(_Math.floor(0.046, 2)).to.equal(0.04)
+  })
+
+  it('Takes 0 as the default for precision, rounding down to the nearest integer.', function() {
+    expect(_Math.floor(4.006)).to.equal(4)
+  })
+
+  it('Takes a negative integer for precision and rounds to a corresponding place.', function() {
+    expect(_Math.floor(4060, -2)).to.equal(4000)
   })
 })
