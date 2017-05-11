363c2a128953424b8b78cf28a2e7e72893c7e9cf
Learner <learner@LGs-MacBook-Pro.local>
2017-05-11 15:30:21 -0700

    finished _.lowerCase

--- test/string_test.js
+++ test/string_test.js
@@ -1,8 +1,24 @@
 import {expect} from 'chai'
 import _String from '../src/string.js'
 
-describe.only('_String.capitalize', () => {
+describe('_String.capitalize', () => {
   it('Converts the first letter of a string to upper case and the remaining to lower case', function() {
     expect(_String.capitalize('FRED')).to.equal('Fred')
   })
+})
+
+describe.only('_String.lowerCase', () => {
+  it('Converts string as space seperated words, to lower case', function() {
+    expect(_String.lowerCase('---Foo-Bar---')).to.equal('foo bar')
+  })
+  it('Converts string to lowercase, words seperated by space', function() {
+    expect(_String.lowerCase('fooBar')).to.equal('foo bar')
+  })
+  it('Converts string with dashes to lowercase, removes dashes', function() {
+    expect(_String.lowerCase('__FOO_BAR__')).to.equal('foo bar')
+  })
+})
+
+describe('_String.lowerFirst', () => {
+  it('Converts the first letter of a string to lowercase')
 })
