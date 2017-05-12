f28e1499268a21bbb21fd2512d3a0a06765abb81
allyface <ally.b.kendall@gmail.com>
2017-05-11 17:14:44 -0700

    update readme with file locations of all methods

--- src/string.js
+++ src/string.js
@@ -7,24 +7,25 @@ export default class _String {
   static lowerCase(str) {
     str = str.replace(/\W/g, ' ')
              .replace(/_/g, ' ')
-  	for(let i = 0; i < str.length; i++) {
-  	  if ((	str.charAt(i) === str.charAt(i).toLowerCase()) &&
-  	    (str.charAt(i + 1) === str.charAt(i + 1).toUpperCase())) {
-  		  str = str.slice(0, i + 1) + ' ' + str.slice(i + 1)
-  		  return str.toLowerCase()
-  		            .trim()
-  	  }
-  	}
+    for(let i = 0; i < str.length; i++) {
+      if ((	str.charAt(i) === str.charAt(i).toLowerCase()) &&
+      (str.charAt(i + 1) === str.charAt(i + 1).toUpperCase())) {
+        str = str.slice(0, i + 1) + ' ' + str.slice(i + 1)
+        return str.toLowerCase()
+                  .trim()
+      }
+    }
   }
+
   static lowerFirst(str) {
     return str.charAt(0).toLowerCase() + str.slice(1)
   }
+
   static repeat(string, num) {
     if (num > 0){
       return string.repeat(num)
+    } else {
+      return ''
     }
-      else {
-        return ""
-      }
   }
 }
